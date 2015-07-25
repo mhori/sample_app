02ab7fd8bbdb3f947c73fa9e3e3cbe7d96604a5f
mhori <your.mhori@hera.eonet.ne.jp>
2015-07-25 17:05:35 +0900

    Make a basic User model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,8 +2,32 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+   
+
 
    subject { page }
+
+   it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))# ここにコードを記入
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))# ここにコードを記入
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))# ここにコードを記入
+    click_link "sample app"
+    expect(page).to have_title(full_title(''))# ここにコードを記入
+  end
+
+
+   shared_examples_for "all static pages" do
+
+    it { should have_content(heading) }
+    it { should have_title(full_title(page_title)) }
+  end
 
   describe "Home page" do
     before { visit root_path }
@@ -34,7 +58,7 @@ describe "StaticPages" do
 
 
 
-    describe "Contact" do
+    describe "Contact page" do
 
     before { visit contact_path }
 
@@ -42,5 +66,5 @@ describe "StaticPages" do
     it { should have_title(full_title('Contact')) }
    end
 
-
+   
 end
